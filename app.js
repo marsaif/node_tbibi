487072f8489807f19df238329c2757fa52419cf6
= <=>
2022-04-03 16:37:14 +0100

    add test app

--- app.js
+++ app.js
@@ -1,68 +1,68 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require('mongoose');
-var config = require('./database/db.json');
-var cors = require('cors')
-var io = require('./socket')
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var config = require("./database/db.json");
+var cors = require("cors");
+var io = require("./socket");
 
-const passport = require('passport')
-require('dotenv').config()
+const passport = require("passport");
+require("dotenv").config();
 
-mongoose.connect(
-  config.mongo.uri,
-  ()=> { console.log("Connected to DATABASE")});
-  
-var indexRouter = require('./routes/indexRoutes');
-var usersRouter = require('./routes/usersRoutes');
-var appointmentsRouter = require('./routes/appointmentsRoutes');
-var reclamationRouter = require('./routes/reclamationsRoutes');
-var reviewRouter = require('./routes/reviewsRoutes');
-var medicalRecordRouter = require('./routes/medicalRecordRoutes');
-var PaymentRoutes = require('./routes/PaymentRoutes');
+mongoose.connect(config.mongo.uri, () => {
+  console.log("Connected to DATABASE");
+});
+
+var indexRouter = require("./routes/indexRoutes");
+var usersRouter = require("./routes/usersRoutes");
+var appointmentsRouter = require("./routes/appointmentsRoutes");
+var reclamationRouter = require("./routes/reclamationsRoutes");
+var reviewRouter = require("./routes/reviewsRoutes");
+var medicalRecordRouter = require("./routes/medicalRecordRoutes");
+var PaymentRoutes = require("./routes/PaymentRoutes");
+var testRoutes = require("./routes/testRoutes");
 
 var app = express();
-app.use(cors())
+app.use(cors());
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use("/appointments",appointmentsRouter)
-app.use("/reviews",reviewRouter)
-app.use("/reclamations",reclamationRouter)
-app.use("/medicalrecord",medicalRecordRouter)
-app.use("/payments",PaymentRoutes)
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/appointments", appointmentsRouter);
+app.use("/reviews", reviewRouter);
+app.use("/reclamations", reclamationRouter);
+app.use("/medicalrecord", medicalRecordRouter);
+app.use("/payments", PaymentRoutes);
+app.use("/test", testRoutes);
 
-
-app.use(passport.initialize())
-require('./security/passport')(passport)
+app.use(passport.initialize());
+require("./security/passport")(passport);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
-
 
 module.exports = app;
---
+++ routes/testRoutes.js
@@ -0,0 +1,46 @@
+var express = require("express");
+var router = express.Router();
+const Appointment = require("../models/appointment");
+const User = require("../models/user");
+
+router.post("/", async function (req, res, next) {
+  try {
+    const doctor = await User.findById({ _id: req.body.doctor });
+    const patient = await User.findById({ _id: req.body.patient });
+    const date = new Date();
+
+    const appointment = new Appointment({
+      doctor: doctor,
+      patient: patient,
+      DateAppointment: date,
+    });
+    appointment.save();
+    res.send("saved");
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+router.get("/:user_id", async function (req, res, next) {
+  try {
+    const user = await User.findById({ _id: req.params.user_id });
+    if (user.role == "DOCTOR") {
+      const appointments = await Appointment.find({ doctor: user })
+        .populate("doctor")
+        .populate("patient")
+        .exec();
+
+      res.json({ appointments: appointments });
+    } else {
+      const appointments = await Appointment.find({ patient: user })
+        .populate("doctor")
+        .populate("patient")
+        .exec();
+      res.json({ appointments: appointments });
+    }
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+module.exports = router;
