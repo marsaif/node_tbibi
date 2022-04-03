3ce76b7b85add971c35fdc0001ad839bea8f926d
lotfi-bsir <lotfi.bsir@esprit.tn>
2022-04-03 23:18:18 +0100

    add appointment models and routes

--- models/appointment.js
+++ models/appointment.js
@@ -5,7 +5,10 @@ var Schema = mongoose.Schema;
 var Appointment = new Schema(
   {
     DateAppointment: { type: Date, required: true },
-    patient: { type: Schema.Types.ObjectId, ref: "users" },
+    patientName: { type: String },
+    patientAge: { type: Number },
+    patientPhone: { type: String },
+    gender: { type: String },
     doctor: { type: Schema.Types.ObjectId, ref: "users" },
   },
   { timestamps: true }
--- routes/appointmentsRoutes.js
+++ routes/appointmentsRoutes.js
@@ -1,35 +1,26 @@
-var express = require('express');
-const res = require('express/lib/response');
-const app = require('../app');
-const Appointment = require('../models/appointment');
+var express = require("express");
+const res = require("express/lib/response");
+const app = require("../app");
+const Appointment = require("../models/appointment");
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-    Appointment.find({}, function(err, appointments) {
-    res.send(appointments)
-    });
+router.get("/", function (req, res, next) {
+  Appointment.find({}, function (err, appointments) {
+    res.send(appointments);
+  });
 });
 
-router.post("/",function(req, res, next) {
-  
-  const appo = new Appointment({ DateAppointment: req.body.name });
-  appo.save().then(() => console.log('meow'));
-  res.send('nice');
+router.post("/", async function (req, res) {
+  const appointment = new Appointment({ ...req.body });
+  await appointment.save();
+  res.status(201).send(appointment);
 });
 
-router.delete('/',function(req, res, next) {
-  id = req.body.id
-  Appointment.findByIdAndDelete(id,(err,data) => {
-    res.send("data deleted"+data) ;
-  }) ;
-}) ;
-
-router.put("/",function(req, res, next) {
-  id = req.body.id
-  Appointment.findByIdAndUpdate(id,{ DateAppointment: 'foo' },(err,data) => {
-    res.send("data updated") ;
-  }) ;
-}) ;
-
+router.put("/", function (req, res, next) {
+  id = req.body.id;
+  Appointment.findByIdAndUpdate(id, { DateAppointment: "foo" }, (err, data) => {
+    res.send("data updated");
+  });
+});
 
 module.exports = router;
